5a51a411d5fb4924629fa18f04cdcecda7a1fa13
AshimDhakal7 <np03cs4a220256@heraldcollege.edu.np>
2026-04-05 16:38:52 +0545

    Solved cors error

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API = import.meta.env.VITE_API_URL || "http://localhost:5001";
 
 console.log("🔌 CricBook API base URL:", API);
 
